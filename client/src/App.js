a6e2337cd813e2a80c82ac5ce05f549dd524c96d
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2023-10-26 23:41:52 +0530

    clent side

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Layout from './components/Layout/Layout';
+function App() {
+  return (
+    <>
+    <Layout>
+
+    <h1>Sugar</h1>
+    
+    
+    
+    </Layout>
+
+      
+
+    </>
+  );
+}
+
+export default App;
